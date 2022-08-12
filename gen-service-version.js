157ae0882719986606e20bbd0cdb05190a307429
Nils Heydecker <48135649+Nilstrieb@users.noreply.github.com>
2022-08-12 14:56:03 +0200

    it's fine 2

--- gen-service-version.js
+++ gen-service-version.js
@@ -1,24 +1,26 @@
-const child_process = require('child_process');
+const child_process = require("child_process");
 
-const services = ['bar-service', 'foo-service'];
+const services = ["bar-service", "foo-service"];
 
 function latestCommitInDirectory(dirname) {
-    return String(child_process.execSync(`git log --pretty=format:%H -n 1 -- ${dirname}`));
+  return String(
+    child_process.execSync(`git log --pretty=format:%H -n 1 -- ${dirname}`)
+  );
 }
 
 const serviceVersions = {};
 
-const currentCommit = latestCommitInDirectory('.');
+const currentCommit = latestCommitInDirectory(".");
 
 for (const service of services) {
-    serviceVersions[service] = latestCommitInDirectory(service);
+  serviceVersions[service] = latestCommitInDirectory(service);
 }
 
 console.log(currentCommit);
 console.log(serviceVersions);
 
 Object.entries(serviceVersions).forEach(([name, version]) => {
-    const skip = version === currentCommit ? 'run' : 'skip';
+  const skip = version === currentCommit ? "run" : "skip";
 
-    console.log(`::set-output name=${name}::${skip}`)
+  console.log(`::set-output name=${name}::${skip}`);
 });
